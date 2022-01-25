5e2cebd38022f3b96af7cd10101a002b5fa4d0bf
Debian <bitnami@ip-172-26-12-2.eu-west-1.compute.internal>
2022-01-25 19:14:08 +0000

    server side settings

--- forest_site/forest_site/settings.py
+++ forest_site/forest_site/settings.py
@@ -26,15 +26,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r236=e_a(uuu3@_+39cl#dws^@fd+iq@k%ls7=ct+58m)sszr='
-        #'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag'
+#SECRET_KEY = 'r236=e_a(uuu3@_+39cl#dws^@fd+iq@k%ls7=ct+58m)sszr='
+SECRET_KEY = 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag'
 
 
-SECRET_KEY = [os.environ.get('SECRET_KEY')]
+#SECRET_KEY = [os.environ.get('SECRET_KEY')]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = os.getenv('DEBUG', '') != 'False'
+DEBUG = True
+#DEBUG = os.getenv('DEBUG', '') != 'False'
 #
 
 
@@ -43,7 +43,7 @@ DEBUG = os.getenv('DEBUG', '') != 'False'
 #ALLOWED_HOSTS = [str(os.getenv('ALLOWED_HOSTS'))]
 ALLOWED_HOSTS = ['52.18.34.67']
 #print("hosts here "+hosts)
-
+#
 # Application definition
 
 INSTALLED_APPS = [
@@ -146,18 +146,21 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "forest_site/static"),
+    os.path.join(BASE_DIR, "static/"),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'forest_site/static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'forest_site/static')
 
 # Base url to serve media files
-#MEDIA_URL = '/static/'
+MEDIA_URL = '/media/'
 
 # Path where media is stored
-MEDIA_ROOT = os.path.join(BASE_DIR, 'forest_site/static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 #True to avoid transmitting the CSRF cookie over HTTP accidentally
-CSRF_COOKIE_SECURE = True
+#CSRF_COOKIE_SECURE = False
 #True to avoid transmitting the session cookie over HTTP accidentally
-SESSION_COOKIE_SECURE = True
+#SESSION_COOKIE_SECURE = False
+#CSRF_COOKIE_DOMAIN = '52.28.34.67'
+#CSRF_TRUSTED_ORIGINS = '52.28.34.67'
+#SECURE_PROXY_HANDLER = ('HTTP_X_FORWARDED_PROTO'. 'http')
