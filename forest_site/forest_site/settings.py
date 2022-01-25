f958a5afea51a2399c9e4b7cfc4312b484079ec8
gallac36 <conor.gallagher2@mail.dcu.ie>
2022-01-25 20:05:53 +0000

    merging changes in settings

--- forest_site/forest_site/settings.py
+++ forest_site/forest_site/settings.py
@@ -26,13 +26,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-<<<<<<< HEAD
+
 #SECRET_KEY = 'r236=e_a(%kv3@_+39cl#dws^@fd+iq@k%ls7=ct+58m)sszr='
         #'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag'
-=======
+
 #SECRET_KEY = 'r236=e_a(uuu3@_+39cl#dws^@fd+iq@k%ls7=ct+58m)sszr='
 SECRET_KEY = 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag'
->>>>>>> 5e2cebd38022f3b96af7cd10101a002b5fa4d0bf
+
 
 
 #SECRET_KEY = [os.environ.get('SECRET_KEY')]
@@ -40,11 +40,7 @@ SECRET_KEY = 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 #DEBUG = os.getenv('DEBUG', '') != 'False'
-<<<<<<< HEAD
 
-=======
-#
->>>>>>> 5e2cebd38022f3b96af7cd10101a002b5fa4d0bf
 
 
 #ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
@@ -163,20 +159,20 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 
 # Path where media is stored
-<<<<<<< HEAD
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'forest_site/media')
-=======
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
->>>>>>> 5e2cebd38022f3b96af7cd10101a002b5fa4d0bf
+
 
 #True to avoid transmitting the CSRF cookie over HTTP accidentally
 #CSRF_COOKIE_SECURE = False
 #True to avoid transmitting the session cookie over HTTP accidentally
-<<<<<<< HEAD
+
 SESSION_COOKIE_SECURE = True
-=======
+
 #SESSION_COOKIE_SECURE = False
 #CSRF_COOKIE_DOMAIN = '52.28.34.67'
 #CSRF_TRUSTED_ORIGINS = '52.28.34.67'
 #SECURE_PROXY_HANDLER = ('HTTP_X_FORWARDED_PROTO'. 'http')
->>>>>>> 5e2cebd38022f3b96af7cd10101a002b5fa4d0bf
+
